7b019fb2c8a8e3c07f557c0fcf69876345bcedeb
monukumar98 <monu012as@gmail.com>
2026-02-15 15:21:49 +0530

    LinkedList

---
+++ Lec27/LinkedList_Client.java
@@ -0,0 +1,15 @@
+package Lec27;
+
+public class LinkedList_Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList ll = new LinkedList();
+		ll.AddFirst(10);
+		ll.AddFirst(20);
+		ll.AddFirst(30);
+		ll.Display();
+		
+	}
+
+}
