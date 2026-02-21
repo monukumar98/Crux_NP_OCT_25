7ed054ba2be841fbedd26b9be6ce5c170a12f16b
monukumar98 <monu012as@gmail.com>
2026-02-21 15:38:53 +0530

    LinkedList

--- Lec28/Reverse_Nodes_in_k_Group.java
+++ Lec28/Reverse_Nodes_in_k_Group.java
@@ -1,5 +1,7 @@
 package Lec28;
 
+import java.util.Stack;
+
 public class Reverse_Nodes_in_k_Group {
 
 	public class ListNode {
@@ -21,25 +23,36 @@ public class Reverse_Nodes_in_k_Group {
 
 	class Solution {
 		public ListNode reverseKGroup(ListNode head, int k) {
+			Stack<ListNode> st = new Stack<>();
+			ListNode dummy = new ListNode();
+			ListNode temp = dummy;
+			ListNode prev = head;
+			while (head != null) {
+				// stack me k element add krna
+				int c = 0;
+				while (head != null && c < k) {
+					st.push(head);
+					head = head.next;
+					c++;
+				}
+
+				// reverse attach krna
+				if (c == k) {
+					while (!st.isEmpty()) {
+						dummy.next = st.pop();
+						dummy = dummy.next;
+					}
+					  dummy.next=null;
+				}
+				else {
+					dummy.next=prev;
+				}
+				prev = head;
+
+			}
+			return temp.next;
 
 		}
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
